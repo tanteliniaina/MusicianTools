f7e61d9da575e6ffe9ff9a9f661661a3b47ba32f
tantely <jeffycessna@gmail.ccom>
2023-09-26 00:22:04 +0200

    creation de toutes les interfaces utilisateurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AllComponent } from './libraries/view/all/all.component';
+import { ShareComponent } from './libraries/operation/share/share.component';
+import { DetailsComponent } from './libraries/view/details/details.component';
+import { AuthentificationComponent } from './account/authentification/authentification.component';
+import { HomeComponent } from './general/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AllComponent,
+    ShareComponent,
+    DetailsComponent,
+    AuthentificationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
